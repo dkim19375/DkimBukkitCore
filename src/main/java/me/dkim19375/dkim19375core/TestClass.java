7f1af374a118c4d2656e05702b77dc2733d31fd6
dkim19375 <miner8mk@gmail.com>
2020-12-29 17:33:33 -0500

    Add sender method

--- src/main/java/me/dkim19375/dkim19375core/TestClass.java
+++ src/main/java/me/dkim19375/dkim19375core/TestClass.java
@@ -1,7 +1,12 @@
 package me.dkim19375.dkim19375core;
 
+import org.bukkit.command.CommandSender;
+
 public class TestClass {
     public static void testPrint() {
         System.out.println("TESTING");
+    }
+    public static void testPrint(CommandSender sender) {
+        sender.sendMessage("TESTING");
     }
 }
