032e784579bb18ae508bc1eeb30a549a7d5913c2
dkim19375 <miner8mk@gmail.com>
2021-01-02 18:54:25 -0500

    Add Consumers

---
+++ src/main/java/me/dkim19375/dkim19375core/consumers/QuadConsumer.java
@@ -0,0 +1,55 @@
+package me.dkim19375.dkim19375core.consumers;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * Represents an operation that accepts two input arguments and returns no
+ * result.  This is the two-arity specialization of {@link Consumer}.
+ * Unlike most other functional interfaces, {@code QuadConsumer} is expected
+ * to operate via side-effects.
+ *
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #accept(Object, Object, Object, Object)}.
+ *
+ * @param <T> the type of the first argument to the operation
+ * @param <U> the type of the second argument to the operation
+ *
+ * @see Consumer
+ * @see java.util.function.BiConsumer
+ * @see TriConsumer
+ */
+@FunctionalInterface
+public interface QuadConsumer<T, U, V, W> {
+
+    /**
+     * Performs this operation on the given arguments.
+     *
+     * @param t the first input argument
+     * @param u the second input argument
+     * @param v the third input argument
+     * @param w the fourth input argument
+     */
+    void accept(T t, U u, V v, W w);
+
+    /**
+     * Returns a composed {@code QuadConsumer} that performs, in sequence, this
+     * operation followed by the {@code after} operation. If performing either
+     * operation throws an exception, it is relayed to the caller of the
+     * composed operation.  If performing this operation throws an exception,
+     * the {@code after} operation will not be performed.
+     *
+     * @param after the operation to perform after this operation
+     * @return a composed {@code QuadConsumer} that performs in sequence this
+     * operation followed by the {@code after} operation
+     * @throws NullPointerException if {@code after} is null
+     */
+    default QuadConsumer<T, U, V, W> andThen(QuadConsumer<? super T, ? super U, ? super V, ? super W> after) {
+        Objects.requireNonNull(after);
+
+        return (l, lm, m, r) -> {
+            accept(l, lm, m, r);
+            after.accept(l, lm, m, r);
+        };
+    }
+}
