e8160cf87936a45fabf5d6fe688f2f974ca123d2
dkim19375 <miner8mk@gmail.com>
2021-01-12 18:07:55 -0500

    Add NumberUtils - getPercentage and chance

---
+++ src/main/java/me/dkim19375/dkim19375core/NumberUtils.java
@@ -0,0 +1,19 @@
+package me.dkim19375.dkim19375core;
+
+public class NumberUtils {
+    public static long getPercentage(long lower, long max) {
+        return (lower * 100) / max;
+    }
+
+    public static double getPercentageDouble(double lower, double max) {
+        return (lower * 100) / max;
+    }
+
+    public static boolean percentChance(float chance) {
+        return Math.random() <= chance;
+    }
+
+    public static boolean percentChance(int chance) {
+        return Math.random() <= chance;
+    }
+}
