02cbabf71534288ea9567e771ad2bd01da5906ce
dkim19375 <miner8mk@gmail.com>
2021-01-26 17:53:45 -0500

    Add a better random number generator

--- src/main/java/me/dkim19375/dkim19375core/NumberUtils.java
+++ src/main/java/me/dkim19375/dkim19375core/NumberUtils.java
@@ -1,19 +1,17 @@
 package me.dkim19375.dkim19375core;
 
+import java.util.Random;
+
 public class NumberUtils {
-    public static long getPercentage(long lower, long max) {
-        return (lower * 100) / max;
-    }
+    private final static Random random = new Random();
 
-    public static double getPercentageDouble(double lower, double max) {
-        return (lower * 100) / max;
-    }
+    public static long getPercentage(long lower, long max) { return (lower * 100) / max; }
 
-    public static boolean percentChance(float chance) {
-        return Math.random() * 100 <= chance;
-    }
+    public static double getPercentageDouble(double lower, double max) { return (lower * 100) / max; }
 
-    public static boolean percentChance(int chance) {
-        return Math.random() * 100 <= chance;
-    }
+    public static boolean percentChance(float chance) { return Math.random() * 100 <= chance; }
+
+    public static boolean percentChance(int chance) { return Math.random() * 100 <= chance; }
+
+    public static int getRandomNumber(int minimum, int maximum) { return random.nextInt(maximum - minimum + 1) + maximum; }
 }
