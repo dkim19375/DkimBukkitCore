647094e01021c60b2e17d5a5945816c4411dd84a
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2022-07-11 17:11:31 -0400

    Update DkimCore

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "me.dkim19375"
-version = "3.3.40"
+version = "3.3.41"
 
 val javaVersion = "1.8"
 
@@ -32,7 +32,7 @@ repositories {
 }
 
 dependencies {
-    api("io.github.dkim19375:dkimcore:1.3.2")
+    api("io.github.dkim19375:dkimcore:1.3.3")
     api("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
 
     compileOnly("me.clip:placeholderapi:2.11.2")
